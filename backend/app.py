7d4fbfa3e191d1b91838fb4e889c926a8baffbd3
Jason Antwi-Appah <hey@jasonaa.me>
2023-04-16 03:42:27 -0500

    feat: caching, perf improvements

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 from respell import *
 from dotenv import load_dotenv
+
+from starlette.requests import Request
+from starlette.responses import Response
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from fastapi_cache.coder import JsonCoder
+from redis import asyncio as aioredis
 
 load_dotenv() 
 app = FastAPI()
@@ -14,8 +23,26 @@ def read_root():
 class RecipeBody(BaseModel):
     ingredients: list[str]
 
+EXPIRE = 60*60*24
+
+def key_builder(
+        func,
+        namespace: Optional[str] = "",
+        request: Optional[Request] = None,
+        response: Optional[Response] = None,
+        *args,
+        **kwargs,
+):
+    prefix = FastAPICache.get_prefix()
+    use = kwargs["kwargs"]
+    del use['req']
+    cache_key = f"{prefix}:{namespace}:{func.__module__}:{func.__name__}:{use}"
+    print("cache_key", cache_key)
+    return cache_key
+
 @app.post("/image")
-def gen_image(recipe_title: str):
+@cache(expire=EXPIRE, coder=JsonCoder)
+async def gen_image(req: Request, recipe_title: str):
     return generate_image(recipe_title)
 
 class GenRecipesWithImagesOutput(BaseModel):
@@ -27,11 +54,22 @@ class GenRecipesOutput(BaseModel):
     recipes_with_limited_ingredients: list[FormattedRecipe]
 
 @app.post("/recipes")
-def gen_recipes(body: RecipeBody) -> GenRecipesOutput:
-    extra, limited = generate_recipes(body.ingredients, False)
+@cache(expire=EXPIRE, coder=JsonCoder)
+async def gen_recipes(req: Request, b: RecipeBody) -> GenRecipesOutput:
+    extra, limited = generate_recipes(b.ingredients, False)
     return GenRecipesOutput(recipes_with_extra_ingredients=extra, recipes_with_limited_ingredients=limited) # type: ignore
 
 @app.post("/recipes_with_images")
-def gen_recipes_with_images(body: RecipeBody) -> GenRecipesWithImagesOutput:
-    extra, limited = generate_recipes(body.ingredients, True)
+@cache(expire=EXPIRE, coder=JsonCoder)
+async def gen_recipes_with_images(req: Request, b: RecipeBody) -> GenRecipesWithImagesOutput:
+    extra, limited = generate_recipes(b.ingredients, True)
     return GenRecipesWithImagesOutput(recipes_with_extra_ingredients=extra, recipes_with_limited_ingredients=limited) # type: ignore
+
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url(os.environ["REDIS_URL"], encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache", key_builder=key_builder)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- backend/respell.py
+++ backend/respell.py
@@ -1,16 +1,17 @@
+from multiprocessing import Pool
 from uuid import uuid4
 from pydantic import BaseModel
 import requests
 import json
 import os
 
-prompt_base = "I want you to act as a chef with expertise in cooking and menu preparation. I will give you a list of ingredients that I currently have. Non-perishable items, such as pasta, rice, or condiments like salt and pepper, are always available."
+prompt_base = "I want you to act as a chef. I will give you a list of ingredients that I currently have. Non-perishable items, such as pasta, rice, or condiments like salt and pepper, are always available."
 
-prompt_suffix = " Break each recipe down into steps that can be easily followed by an inexperienced chef. Focus on the flavors and textures of the ingredients, and provide step-by-step instructions for each recipe. The first line of each recipe should be the title of the recipe. The second line should be a comma delimited list of ingredients, with no spaces. Do not use multiple lines to list out the ingredients. All subsequent lines should consist of the steps for the recipe. Do not include any section headings. Separate each recipe with 3 dashes."
+prompt_suffix = " Break each recipe down into step-by-step instructions that can be followed by an inexperienced cook. The first line of each recipe should be the title of the recipe. The second line should be a comma delimited list of ingredients, with no spaces. Do not use multiple lines to list out the ingredients. All subsequent lines should consist of the steps for the recipe. Do not include any section headings. Separate each recipe with 3 dashes."
 
-propmt_use_limited_ingredients = prompt_base + " Create 6 original recipes, using only the ingredients I currently have. Do not include any extra ingredients in the recipes." + prompt_suffix
+propmt_use_limited_ingredients = prompt_base + " Create 6 recipes, using only the ingredients I currently have. Do not include other perishable ingredients in the recipes." + prompt_suffix
 
-prompt_use_extra_ingredients = prompt_base + "Create 6 original recipes, which include some or all of the ingredients I currently have. Let the recipes be flavorful and delicious, using herbs, spices, and other additives to enhance the recipes. Feel free to be creative and include ingredients I do not have in the list." + prompt_suffix
+prompt_use_extra_ingredients = prompt_base + "Create 3 recipes, which include some or all of the ingredients I currently have. Use additional ingredients to enhance the recipes. Feel free to be creative." + prompt_suffix
 
 def generate_image(recipe_title: str) -> str:
     response = requests.post(
@@ -63,10 +64,9 @@ def generate_recipes(ingredients: list[str], include_images = False):
     extra, current = out
     extra_with_images: list[FormattedRecipeWithImage] = []
     current_with_images: list[FormattedRecipeWithImage] = []
-    for recipe in extra:
-        extra_with_images.append(FormattedRecipeWithImage(id=recipe.id, title=recipe.title, ingredients=recipe.ingredients, instructions=recipe.instructions, image_url=generate_image(recipe.title)))
-    for recipe in current:
-        current_with_images.append(FormattedRecipeWithImage(id=recipe.id, title=recipe.title, ingredients=recipe.ingredients, instructions=recipe.instructions, image_url=generate_image(recipe.title)))
+    with Pool(5) as p:
+        extra_with_images = p.map(add_image, extra)
+        current_with_images = p.map(add_image, current)
 
     return tuple([extra_with_images, current_with_images])
 
@@ -92,7 +92,6 @@ def _raw_recipe_to_formatted(raw_recipe: str) -> FormattedRecipe:
     split = raw_recipe.strip().split("\n")
     print(split)
     title = split[0].split(":")[-1].strip()
-    print(split)
     ingredients = split[1].strip().split(",")
     steps = split[2:]
     return FormattedRecipe(title=title, ingredients=ingredients, instructions=steps, id=str(uuid4()))
@@ -104,11 +103,22 @@ def _parse_raw_recipes(raw_recipe: RawRecipeOutput) -> tuple[list[FormattedRecip
     for recipe in raw_recipe.outputs.recipes_with_extra_ingredients.split("---"):
         if len(recipe.strip().split("\n")) < 3:
             continue
-        recipes_with_extra_ingredients.append(_raw_recipe_to_formatted(recipe))
+        fmt = _raw_recipe_to_formatted(recipe)
+        for r in recipes_with_extra_ingredients:
+            if r.title == fmt.title:
+                continue
+        recipes_with_extra_ingredients.append(fmt)
 
     for recipe in raw_recipe.outputs.recipes_with_current_ingredients.split("---"):
         if len(recipe.strip().split("\n")) < 3:
             continue
-        recipes_with_current_ingredients.append(_raw_recipe_to_formatted(recipe))
+        fmt = _raw_recipe_to_formatted(recipe)
+        for r in recipes_with_extra_ingredients + recipes_with_current_ingredients:
+            if r.title == fmt.title:
+                continue
+        recipes_with_current_ingredients.append(fmt)
 
     return tuple([recipes_with_extra_ingredients, recipes_with_current_ingredients])
+
+def add_image(recipe: FormattedRecipe) -> FormattedRecipeWithImage:
+    return FormattedRecipeWithImage(id=recipe.id, title=recipe.title, ingredients=recipe.ingredients, instructions=recipe.instructions, image_url=generate_image(recipe.title))
