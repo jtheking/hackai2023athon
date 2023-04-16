41dc4948c33378e00ae249cd32dcfbe0009d0f2b
Jason Antwi-Appah <hey@jasonaa.me>
2023-04-16 08:18:22 -0500

    dedupe ingredients

--- backend/app.py
+++ backend/app.py
@@ -10,8 +10,13 @@ from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
 from fastapi_cache.coder import JsonCoder
 from redis import asyncio as aioredis
+import time
 
 load_dotenv() 
+
+import boto3
+import numpy as np, cv2
+
 app = FastAPI()
 
 
@@ -77,10 +82,39 @@ async def startup():
     redis = aioredis.from_url(os.environ["REDIS_URL"], encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache", key_builder=key_builder)
 
+@app.get("/lol")
+def hey():
+    ENDPOINT_NAME = 'yolov5l-demo'
+    # config = botocore.config.Config(read_timeout=80)
+    # runtime = boto3.client('runtime.sagemaker', config=config)
+    # runtime = boto3.Session().client('sagemaker')
+    # runtime = boto3.client('runtime.sagemaker')
+    runtime = boto3.client('sagemaker-runtime', region_name="us-east-1")
+    
+    modelHeight, modelWidth = 640, 640
+    file_path = "/Users/json/Documents/src/jtheking/hackai2023athon/backend/1.png"
+    orig_image = cv2.imread(file_path)
+    out = None
+    if orig_image is not None:
+        start_time_iter = time.time()
+        # pre-processing input image
+        image = cv2.resize(orig_image.copy(), (modelWidth, modelHeight), interpolation = cv2.INTER_AREA)
+        data = np.array(image.astype(np.float32)/255.)
+        payload = json.dumps([data.tolist()])
+        # write payload to a text file
+        # run inference
+        response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME, ContentType='application/json', Body=payload)
+        # get the output results
+        out = json.loads(response['Body'].read().decode())
+        end_time_iter = time.time()
+        # get the total time taken for inference
+        inference_time = round((end_time_iter - start_time_iter)*100)/100
+    return out
+
 @app.post("/set_ingredients")
 def set_ingredients(i: list[str]):
     global ingredients
-    ingredients = i
+    ingredients = list(set(i))
     return ingredients
 
 if __name__ == "__main__":
